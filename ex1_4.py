a90e7a41ecc519baab317cc93e330c8b9ec6bf65
FireCoder-N <dimnestmanolis@gmail.com>
2023-03-02 16:55:39 +0200

    Exercise1, question 4

---
+++ ex1_4.py
@@ -0,0 +1,23 @@
+import requests
+
+while True:
+    url = input("Enter a url or press enter to exit: ")
+    
+    if url == "":
+        break
+
+    try:
+        cookies = requests.get(url).cookies
+        headers = requests.get(url).headers
+
+        print ("webserver:", headers['Server'])
+        
+        if "Set-Cookie" in headers.keys():
+            print("Website uses the following cookies:")
+            for cookie in cookies:
+                print("\t", cookie, cookie.expires)
+                
+    except requests.exceptions.MissingSchema:
+        print("Invalid url")
+    
+    print("\n")
